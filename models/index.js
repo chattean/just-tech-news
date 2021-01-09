fcabd8c943ce3c437f5a9de70ea61dfb74b6a0f1
Anuj Chatterjee <anujchatterjee@gmail.com>
2021-01-09 00:46:08 -0800

    updated post routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,16 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations
+
+// User has many Post
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+  // Post can only belong to one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 
 module.exports = { User, Post };
